f1e112f1a1a8686816b596998be345ac8143ce45
jgvsolucoes21-tech <jgvsolucoes21@gmail.com>
2025-12-07 13:38:56 -0300

    Update HomeScreen.js

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,12 +1,26 @@
 import { View, Text, StyleSheet } from "react-native";
+import { router } from "expo-router";
+import ButtonPrimary from "../components/ButtonPrimary";
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Americanízate 🇺🇸</Text>
       <Text style={styles.subtitle}>
-        Aprende cómo adaptarte, trabajar y vivir en Estados Unidos.
+        Aprende lo esencial para adaptarte a la vida en Estados Unidos.
       </Text>
+
+      <View style={styles.buttons}>
+        <ButtonPrimary 
+          label="Recursos Útiles" 
+          onPress={() => router.push("/(tabs)/recursos")} 
+        />
+
+        <ButtonPrimary 
+          label="Guía Legal" 
+          onPress={() => router.push("/(tabs)/legal")} 
+        />
+      </View>
     </View>
   );
 }
@@ -29,5 +43,10 @@ const styles = StyleSheet.create({
     fontSize: 16,
     textAlign: "center",
     color: "#555",
+    marginBottom: 20,
+  },
+  buttons: {
+    marginTop: 20,
+    width: "100%",
   },
 });
