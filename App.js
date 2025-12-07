819ed5e61e5a83d91a200719bc37c4e29a8a7385
jgvsolucoes21-tech <jgvsolucoes21@gmail.com>
2025-12-07 13:14:24 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,31 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./screens/HomeScreen";
+import QuizScreen from "./screens/QuizScreen";
+import ResultScreen from "./screens/ResultScreen";
 
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen 
+          name="Home" 
+          component={HomeScreen}
+          options={{ title: "Americanízate" }}
+        />
+        <Stack.Screen 
+          name="Quiz" 
+          component={QuizScreen}
+          options={{ title: "Quiz" }}
+        />
+        <Stack.Screen 
+          name="Result" 
+          component={ResultScreen}
+          options={{ title: "Resultado" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
